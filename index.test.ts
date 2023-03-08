9f8e9d9dd5239bff8cc5d84829017d45083d24b0
Wataru Watanabe <wataru.chocola@gmail.com>
2023-03-08 10:40:21 +0900

    add test case

--- index.test.ts
+++ index.test.ts
@@ -6,9 +6,30 @@ import { toMarkdown as mdastToMarkdown } from "mdast-util-to-markdown";
 import { toMdast as hastToMdast } from "hast-util-to-mdast";
 import { defListToMarkdown } from "mdast-util-definition-list";
 
-test("Basic conversion", () => {
-  const html = `<dl><dt>First Term</dt><dd>This is the <strong>definition</strong> of the first term.</dd><dd>This is another definition of the first term.</dd><dt>Second Term</dt><dd>This is one definition of the second term.</dd><dd>This is another definition of the second term.</dd></dl>`;
+import dedent from "ts-dedent";
 
+test.each([
+  {
+    html: dedent`
+    <dl>
+    <dt>First Term</dt>
+    <dd>This is the <strong>definition</strong> of the first term.</dd>
+    <dd>This is another definition of the first term.</dd>
+    <dt>Second Term</dt>
+    <dd>This is one definition of the second term.</dd>
+    <dd>This is another definition of the second term.</dd>
+    </dl>`,
+    expected: dedent`
+    First Term
+    :   This is the *definition* of the first term.
+    :   This is another definition of the first term.
+
+    Second Term
+    :   This is one definition of the second term.
+    :   This is another definition of the second term.
+    `,
+  },
+])("html => md", ({ html, expected }) => {
   const hast = hastFromHtml(html);
   const mdast = hastToMdast(hast, {
     handlers: {
@@ -18,5 +39,5 @@ test("Basic conversion", () => {
   const md = mdastToMarkdown(mdast, {
     extensions: [defListToMarkdown],
   });
-  expect(md).toBe("");
+  expect(md).toBe(expected);
 });

50f7d789a2407409a58ceb92a719b7460e3fbdb7
Wataru Watanabe <wataru.chocola@gmail.com>
2023-03-09 12:26:48 +0900

    implement handlers

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { definitionListHastToMdast } from "./index.js";
+import { definitionListHastToMdast, dl } from "./index.js";
 
 import { fromHtml as hastFromHtml } from "hast-util-from-html";
 import { toMarkdown as mdastToMarkdown } from "mdast-util-to-markdown";
@@ -10,7 +10,9 @@ import dedent from "ts-dedent";
 
 test.each([
   {
+    title: "simple tight dl",
     html: dedent`
+    <p>This is paragraph.</p>
     <dl>
     <dt>First Term</dt>
     <dd>This is the <strong>definition</strong> of the first term.</dd>
@@ -20,8 +22,10 @@ test.each([
     <dd>This is another definition of the second term.</dd>
     </dl>`,
     expected: dedent`
+    This is paragraph.
+
     First Term
-    :   This is the *definition* of the first term.
+    :   This is the **definition** of the first term.
     :   This is another definition of the first term.
 
     Second Term
@@ -29,8 +33,63 @@ test.each([
     :   This is another definition of the second term.
     `,
   },
-])("html => md", ({ html, expected }) => {
-  const hast = hastFromHtml(html);
+  {
+    title: "item list inside tight dd",
+    html: dedent`
+    <dl>
+    <dt>First Term</dt>
+    <dd>This is the <strong>definition</strong> of the first term.
+    <ul>
+    <li>item1</li>
+    <li>item2</li>
+    </ul>
+    </dd>
+    <dd>This is another definition of the first term.</dd>
+    <dt>Second Term</dt>
+    <dd>This is one definition of the second term.</dd>
+    </dl>`,
+    expected: dedent`
+    First Term
+    :   This is the **definition** of the first term.
+        *   item1
+        *   item2
+    :   This is another definition of the first term.
+
+    Second Term
+    :   This is one definition of the second term.
+    `,
+  },
+  {
+    title: "contains a loose (spread) definition",
+    html: dedent`
+    <dl>
+    <dt>First Term</dt>
+    <dd>
+    <p>This is a loose definition.</p>
+    <ul>
+    <li>item1</li>
+    <li>item2</li>
+    </ul>
+    </dd>
+    <dd>This is another definition of the first term.</dd>
+    <dt>Second Term</dt>
+    <dd>This is one definition of the second term.</dd>
+    </dl>`,
+    expected: dedent`
+    First Term
+
+    :   This is a loose definition.
+
+        *   item1
+        *   item2
+    :   This is another definition of the first term.
+
+    Second Term
+    :   This is one definition of the second term.
+    `,
+  },
+])("html => md: $title", ({ html, expected }) => {
+  const hast = hastFromHtml(html, { fragment: true });
   const mdast = hastToMdast(hast, {
     handlers: {
       ...definitionListHastToMdast,
@@ -39,5 +98,5 @@ test.each([
   const md = mdastToMarkdown(mdast, {
     extensions: [defListToMarkdown],
   });
-  expect(md).toBe(expected);
+  expect(md).toBe(expected + "\n");
 });

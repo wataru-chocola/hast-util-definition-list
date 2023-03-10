462847c4ef86eb7a549d4737e2ed1de6c9c9b8b5
Wataru Watanabe <wataru.chocola@gmail.com>
2023-03-10 11:24:25 +0900

    test: add test cases

--- index.test.ts
+++ index.test.ts
@@ -34,6 +34,24 @@ test.each([
     `,
   },
   {
+    title: "not preserve break-line in dd",
+    html: `
+    <dl>
+    <dt>First Term</dt>
+    <dd>This is the <strong>definition</strong> of the first term.
+    Break lines in definition are not preserved.</dd>
+    <dt>Second Term</dt>
+    <dd>This is one definition of the second term.</dd>
+    </dl>`,
+    expected: `
+    First Term
+    :   This is the **definition** of the first term. Break lines in definition are not preserved.
+
+    Second Term
+    :   This is one definition of the second term.
+    `,
+  },
+  {
     title: "associate multiple terms to a definition",
     html: `
     <dl>
@@ -181,6 +199,126 @@ test.each([
 
     Third Item
     :   Third Description
+    `,
+  },
+  {
+    title: "nested defList",
+    html: `
+    <dl>
+    <dt>Term 1</dt>
+    <dd>
+    <p>This is a definition wrapped by paragraph.</p>
+    <dl>
+    <dt>Nested term 1</dt>
+    <dd>Nested description here.
+    And next line.</dd>
+    <dt>Nested term 2</dt>
+    <dd>Description 2.</dd>
+    </dl></dd>
+    <dd>
+    <p>Description.</p>
+    </dd>
+    <dt>Term 2</dt>
+    <dd>
+    <p>Description.</p></dd>
+    </dl>`,
+    expected: `
+    Term 1
+
+    :   This is a definition wrapped by paragraph.
+
+        Nested term 1
+        :   Nested description here. And next line.
+
+        Nested term 2
+        :   Description 2.
+
+    :   Description.
+
+    Term 2
+
+    :   Description.
+    `,
+  },
+
+  {
+    title: "code block inside dd",
+    html: `
+    <dl>
+    <dt>Term</dt>
+    <dd>
+    <pre><code>import sys
+    sys.stdout.write("hello")
+    </code></pre></dd>
+    </dl>`,
+    expected: `
+    Term
+    :       import sys
+            sys.stdout.write("hello")
+    `,
+  },
+  {
+    title:
+      "defList can contain multiple paragraph and other block-level elements",
+    html: `
+    <dl>
+    <dt>Term 1</dt>
+    <dd>
+    <p>This is a definition with two paragraphs. Lorem ipsum
+    dolor sit amet, consectetuer adipiscing elit. Aliquam
+    hendrerit mi posuere lectus.</p>
+    <p>Vestibulum enim wisi, viverra nec, fringilla in, laoreet
+    vitae, risus.</p>
+    </dd>
+    <dd>
+    <p>Second definition for term 1, also wrapped in a paragraph
+    because of the blank line preceding it.</p>
+    </dd>
+    <dt>Term 2</dt>
+    <dd>
+    <p>This definition has a code block, a blockquote and a list.</p>
+    <pre><code>code block.
+    </code></pre>
+    <blockquote>
+    <p>block quote
+    on two lines.</p>
+    </blockquote>
+    <ol>
+    <li>first list item</li>
+    <li>second list item</li>
+    </ol></dd>
+    </dl>`,
+    expected: `
+    Term 1
+
+    :   This is a definition with two paragraphs. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam hendrerit mi posuere lectus.
+
+        Vestibulum enim wisi, viverra nec, fringilla in, laoreet vitae, risus.
+
+    :   Second definition for term 1, also wrapped in a paragraph because of the blank line preceding it.
+
+    Term 2
+
+    :   This definition has a code block, a blockquote and a list.
+
+            code block.
+
+        > block quote on two lines.
+
+        1.  first list item
+        2.  second list item
+    `,
+  },
+  {
+    title: `unsafe character`,
+    html: `
+    <p>Not Term</p>
+    <p>:   Not Definition</p>
+    `,
+    expected: `
+    Not Term
+
+    \\: Not Definition
     `,
   },
 ])("html => md: $title", ({ html, expected }) => {

56281dce465cdb24a9123d24d6cce4c1c608323a
Wataru Watanabe <wataru.chocola@gmail.com>
2023-03-10 09:15:52 +0900

    fix: phrasing in dt

--- index.test.ts
+++ index.test.ts
@@ -6,12 +6,12 @@ import { toMarkdown as mdastToMarkdown } from "mdast-util-to-markdown";
 import { toMdast as hastToMdast } from "hast-util-to-mdast";
 import { defListToMarkdown } from "mdast-util-definition-list";
 
-import dedent from "ts-dedent";
+import { dedent } from "ts-dedent";
 
 test.each([
   {
     title: "simple tight dl",
-    html: dedent`
+    html: `
     <p>This is paragraph.</p>
     <dl>
     <dt>First Term</dt>
@@ -21,7 +21,7 @@ test.each([
     <dd>This is one definition of the second term.</dd>
     <dd>This is another definition of the second term.</dd>
     </dl>`,
-    expected: dedent`
+    expected: `
     This is paragraph.
 
     First Term
@@ -34,8 +34,45 @@ test.each([
     `,
   },
   {
+    title: "associate multiple terms to a definition",
+    html: `
+    <dl>
+    <dt>Term 1</dt>
+    <dt>Term 2</dt>
+    <dd>Definition a</dd>
+    <dt>Term 3</dt>
+    <dd>Definition b</dd>
+    </dl>`,
+    expected: `
+    Term 1
+    Term 2
+    :   Definition a
+
+    Term 3
+    :   Definition b
+    `,
+  },
+  {
+    title: "definition term can be decorated",
+    html: `
+    <dl>
+    <dt>A<strong>pp</strong>le</dt>
+    <dd>Pomaceous fruit of plants of the genus Malus in
+    the family Rosaceae.</dd>
+    <dt>Orange</dt>
+    <dd>The fruit of an evergreen tree of the genus Citrus.</dd>
+    </dl>`,
+    expected: `
+    A**pp**le
+    :   Pomaceous fruit of plants of the genus Malus in the family Rosaceae.
+
+    Orange
+    :   The fruit of an evergreen tree of the genus Citrus.
+    `,
+  },
+  {
     title: "item list inside tight dd",
-    html: dedent`
+    html: `
     <dl>
     <dt>First Term</dt>
     <dd>This is the <strong>definition</strong> of the first term.
@@ -48,7 +85,7 @@ test.each([
     <dt>Second Term</dt>
     <dd>This is one definition of the second term.</dd>
     </dl>`,
-    expected: dedent`
+    expected: `
     First Term
     :   This is the **definition** of the first term.
         *   item1
@@ -61,7 +98,7 @@ test.each([
   },
   {
     title: "contains a loose (spread) definition",
-    html: dedent`
+    html: `
     <dl>
     <dt>First Term</dt>
     <dd>
@@ -75,7 +112,7 @@ test.each([
     <dt>Second Term</dt>
     <dd>This is one definition of the second term.</dd>
     </dl>`,
-    expected: dedent`
+    expected: `
     First Term
 
     :   This is a loose definition.
@@ -88,8 +125,66 @@ test.each([
     :   This is one definition of the second term.
     `,
   },
+  {
+    title: "contains a loose (spread) definition",
+    html: `
+    <dl>
+    <dt>First Term</dt>
+    <dd>
+    <p>This is a loose definition.</p>
+    <ul>
+    <li>item1</li>
+    <li>item2</li>
+    </ul>
+    </dd>
+    <dd>This is another definition of the first term.</dd>
+    <dt>Second Term</dt>
+    <dd>This is one definition of the second term.</dd>
+    </dl>`,
+    expected: `
+    First Term
+
+    :   This is a loose definition.
+
+        *   item1
+        *   item2
+    :   This is another definition of the first term.
+
+    Second Term
+    :   This is one definition of the second term.
+    `,
+  },
+  {
+    title: "wrapped in div",
+    html: `
+    <dl>
+      <div>
+        <dt>First Item</dt>
+        <dd>First Description</dd>
+      </div>
+      <div>
+        <dt>Second Item</dt>
+        <dd>Second Description</dd>
+      </div>
+      <div>
+        <dt>Third Item</dt>
+        <dd>Third Description</dd>
+      </div>
+    </dl>
+    `,
+    expected: `
+    First Item
+    :   First Description
+
+    Second Item
+    :   Second Description
+
+    Third Item
+    :   Third Description
+    `,
+  },
 ])("html => md: $title", ({ html, expected }) => {
-  const hast = hastFromHtml(html, { fragment: true });
+  const hast = hastFromHtml(dedent(html), { fragment: true });
   const mdast = hastToMdast(hast, {
     handlers: {
       ...definitionListHastToMdast,
@@ -98,5 +193,5 @@ test.each([
   const md = mdastToMarkdown(mdast, {
     extensions: [defListToMarkdown],
   });
-  expect(md).toBe(expected + "\n");
+  expect(md).toBe(dedent(expected) + "\n");
 });
